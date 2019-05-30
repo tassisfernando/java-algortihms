74f9f1d8950b262dcab1a9797b2dda75be6d8dc5
Tássis Fernando Soares Fernandes <tassis_fernando@hotmail.com>
2019-05-30 08:09:28 -0300

    Adicionando novos algoritmos

--- Lista_Repeticao_2/src/exercicio_80/Exercicio_80.java
+++ Lista_Repeticao_2/src/exercicio_80/Exercicio_80.java
@@ -7,60 +7,72 @@ public class Exercicio_80 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 
-		String nome, sexo, nmAlto = "", nmPesada = "";
+		String nome = "", sexo, nmAlto = "", nmPesada = "";
 		double peso, altura, mediaId = 0, maisAlto = 0, maisPesa = 0;
 		int idade, totalId = 0, qtd = 0;
 
-		System.out.println("Informe o nome do atleta ");
-		nome = scn.next();
-		System.out.println("Informe o sexo: (M ou F) ");
-		sexo = scn.next().toUpperCase();
-		System.out.println("Informe o peso: ");
-		peso = scn.nextDouble();
-		System.out.println("Informe a altura: ");
-		altura = scn.nextDouble();
-		System.out.println("Informe a idade: ");
-		idade = scn.nextInt();
-
 		while (!nome.equals("@")) {
-			totalId += idade;
-			qtd++;
-			if (sexo.equals("M")) {
-				if (altura > maisAlto) {
-					maisAlto = altura;
-					nmAlto = nome;
-				}
-			} else {
-				if (peso > maisPesa) {
-					maisPesa = peso;
-					nmPesada = nome;
-				}
+			System.out.println("Informe o nome do atleta ");
+			nome = scn.next();
+			while (nome.length() < 2 && !nome.equals("@")) {
+				System.out.println("Informe um nome com pelo menos 2 letras.");
+				nome = scn.next();
 			}
 
-			System.out.println("Informe o nome do atleta ");
-			nome = scn.next().toUpperCase();
 			System.out.println("Informe o sexo: (M ou F) ");
 			sexo = scn.next().toUpperCase();
+			while (!sexo.equals("M") && !sexo.equals("F")) {
+				System.out.println("O sexo só pode ser M ou F: ");
+				sexo = scn.next().toUpperCase();
+			}
+
 			System.out.println("Informe o peso: ");
 			peso = scn.nextDouble();
+			while (peso <= 0) {
+				System.out.println("Peso inválido! Informe o peso:");
+				peso = scn.nextDouble();
+			}
+
 			System.out.println("Informe a altura: ");
 			altura = scn.nextDouble();
+			while (altura <= 0) {
+				System.out.println("Altura inválida! Informe a altura: ");
+				altura = scn.nextDouble();
+			}
+
 			System.out.println("Informe a idade: ");
 			idade = scn.nextInt();
+			while (idade <= 0 || idade > 100) {
+				System.out.println("Idade inválida! Informe a idade: ");
+				idade = scn.nextInt();
+			}
+
+			if (!nome.equals("@")) {
+				totalId += idade;
+				qtd++;
+				if (sexo.equals("M")) {
+					if (altura > maisAlto) {
+						maisAlto = altura;
+						nmAlto = nome;
+					}
+				} else {
+					if (peso > maisPesa) {
+						maisPesa = peso;
+						nmPesada = nome;
+					}
+				}
+			}
 		}
+		System.out.println("\n******FIM DA LEITURA DE ATLETAS******");
 		if (qtd > 0) {
 			mediaId = totalId / qtd;
-		}
-
-		if (!nmAlto.equals("") && !nmPesada.equals("")) {
 			System.out.println("O atleta do sexo masculino mais alto é o " + nmAlto + ", que tem " + maisAlto + "m");
-			System.out.println("A atleta do sexo feminino mais pesada é a " + nmPesada + ", que pesa " + maisPesa + "kg");
+			System.out
+					.println("A atleta do sexo feminino mais pesada é a " + nmPesada + ", que pesa " + maisPesa + "kg");
 			System.out.println("A média geral da idade é " + mediaId + " anos.");
 		} else {
 			System.out.println("Não foram informados atletas.");
 		}
 		scn.close();
-
 	}
-
 }
